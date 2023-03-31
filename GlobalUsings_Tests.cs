7fa5aa961d28993bb0813697f92b7aaa786aa129
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2023-03-31 16:03:48 -0700

    Remove M.T/CT.Uwp/WinUI.UI dependency, internalize helpers
    Need to remove the dependency as that can't build the new version of it due to namespace conflicts with the global usings.
    Once we have a package version of the new unified version, we can investigate a better solution for building in both modes.

--- GlobalUsings_Tests.cs
+++ GlobalUsings_Tests.cs
@@ -6,15 +6,9 @@
 // Learn more global using directives at https://docs.microsoft.com/dotnet/csharp/language-reference/keywords/using-directive#global-modifier
 
 #if !WINAPPSDK
-global using Microsoft.Toolkit.Uwp;
-global using Microsoft.Toolkit.Uwp.UI;
-global using Microsoft.Toolkit.Uwp.UI.Helpers;
 global using Windows.UI;
 global using Windows.UI.Core;
 #else
-global using CommunityToolkit.WinUI;
-global using CommunityToolkit.WinUI.UI;
-global using CommunityToolkit.WinUI.UI.Helpers;
 global using Microsoft.UI;
 #endif
 
