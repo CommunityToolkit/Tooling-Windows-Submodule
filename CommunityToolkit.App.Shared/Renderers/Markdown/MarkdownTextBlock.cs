8cfc5502c532a77b35b510126ff44374cb770789
michael-hawker <24302614+michael-hawker@users.noreply.github.com>
2023-04-20 11:39:25 -0700

    Fixes #47 - Wrong preprocessor directive for WASM for Markdown rendering

--- CommunityToolkit.App.Shared/Renderers/Markdown/MarkdownTextBlock.cs
+++ CommunityToolkit.App.Shared/Renderers/Markdown/MarkdownTextBlock.cs
@@ -16,7 +16,7 @@
     #endif
 #endif
 
-#if WASM
+#if HAS_UNO_WASM
 using Markdig;
 using Uno.Foundation.Interop;
 using Uno.UI.Runtime.WebAssembly;
@@ -27,7 +27,7 @@ namespace CommunityToolkit.App.Shared.Renderers;
 /// <summary>
 /// Provide an abstraction around the Toolkit MarkdownTextBlock for both UWP and WinUI 3 in the same namespace (until 8.0) as well as a polyfill for WebAssembly/WASM.
 /// </summary>
-#if WASM
+#if HAS_UNO_WASM
 [HtmlElement("div")]
 public partial class MarkdownTextBlock : TextBlock
 {
