27bb96d14ed6c194e1a90a1a0bb49491d56d47e0
Niels Laute <niels.laute@live.nl>
2023-06-08 16:49:35 +0200

    Removing unused icon helpers

--- CommunityToolkit.App.Shared/Helpers/IconHelper.cs
+++ CommunityToolkit.App.Shared/Helpers/IconHelper.cs
@@ -22,39 +22,4 @@ public static class IconHelper
         }
         return iconElement;
     }
-
-    public static string GetSubcategoryIcon(ToolkitSampleSubcategory subcategory)
-    {
-        string imagePath = string.Empty;
-        switch (subcategory)
-        {
-            case ToolkitSampleSubcategory.None: imagePath = "ms-appx:///Assets/ControlIcons/Control.png"; break;
-            case ToolkitSampleSubcategory.Behaviors: imagePath = "ms-appx:///Assets/ControlIcons/Input.png"; break;
-            case ToolkitSampleSubcategory.Controls: imagePath = "ms-appx:///Assets/ControlIcons/Control.png"; break;
-            case ToolkitSampleSubcategory.Converters: imagePath = "ms-appx:///Assets/ControlIcons/Input.png"; break;
-            case ToolkitSampleSubcategory.Input: imagePath = "ms-appx:///Assets/ControlIcons/Input.png"; break;
-            case ToolkitSampleSubcategory.Layout: imagePath = "ms-appx:///Assets/ControlIcons/Layout.png"; break;
-            case ToolkitSampleSubcategory.Markup: imagePath = "ms-appx:///Assets/ControlIcons/Input.png"; break;
-            case ToolkitSampleSubcategory.Math: imagePath = "ms-appx:///Assets/ControlIcons/Control.png"; break;
-            case ToolkitSampleSubcategory.Media: imagePath = "ms-appx:///Assets/ControlIcons/Control.png"; break;
-            case ToolkitSampleSubcategory.StatusAndInfo: imagePath = "ms-appx:///Assets/ControlIcons/Status.png"; break;
-            case ToolkitSampleSubcategory.Triggers: imagePath = "ms-appx:///Assets/ControlIcons/Input.png"; break;
-        }
-        return imagePath;
-    }
-
-    public static string GetControlIcon(string? icon)
-    { 
-        return "ms-appx:///SourceAssets/" + icon!;
-       // return "ms-appx:///Assets/ControlIcons/default.png";
-        return "ms-appx:///SourceAssets/Animations/samples/Assets/icon.png";
-        if (string.IsNullOrEmpty(icon))
-        {
-            return "ms-appx:///Assets/ControlIcons/default.png";
-        }
-        else
-        {
-            return icon!;
-        }
-    }
 }
