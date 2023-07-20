fc5b9bca3504f57739a2488aac99228766a4a2bd
Arlo Godfrey <arlo.godfrey@outlook.com>
2023-07-20 16:08:05 -0500

    Added MultiTarget missing TFM error messages. Aligned UseTargetFramework params with MultiTarget names.

--- MultiTarget/UseTargetFrameworks.ps1
+++ MultiTarget/UseTargetFrameworks.ps1
@@ -23,7 +23,7 @@
 #>
 Param (
     [Parameter(HelpMessage = "The target frameworks to enable.")]
-    [ValidateSet('all', 'all-uwp', 'wasm', 'uwp', 'winappsdk', 'wpf', 'gtk', 'macos', 'ios', 'droid', 'netstandard')]
+    [ValidateSet('all', 'all-uwp', 'wasm', 'uwp', 'wasdk', 'wpf', 'linuxgtk', 'macos', 'ios', 'android', 'netstandard')]
     [string[]]$targets = @('uwp', 'winappsdk', 'wasm') # default settings
 )
 
@@ -69,7 +69,7 @@ if ($targets.Contains("uwp")) {
     $desiredTfmValues += $UwpTfm;
 }
 
-if ($targets.Contains("winappsdk")) {
+if ($targets.Contains("wasdk")) {
     $desiredTfmValues += $WinAppSdkTfm;
 }
 
@@ -77,7 +77,7 @@ if ($targets.Contains("wpf")) {
     $desiredTfmValues += $WpfTfm;
 }
 
-if ($targets.Contains("gtk")) {
+if ($targets.Contains("linuxgtk")) {
     $desiredTfmValues += $GtkTfm;
 }
 
@@ -89,7 +89,7 @@ if ($targets.Contains("ios")) {
     $desiredTfmValues += $iOSTfm;
 }
 
-if ($targets.Contains("droid")) {
+if ($targets.Contains("android")) {
     $desiredTfmValues += $DroidTfm;
 }
 
