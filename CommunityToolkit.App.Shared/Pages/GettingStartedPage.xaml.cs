bc0cdc58f947e8f95c9e198466d97210e2005767
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2024-07-11 11:28:52 -0700

    Add missing using to Package Versioning for WASDK

--- CommunityToolkit.App.Shared/Pages/GettingStartedPage.xaml.cs
+++ CommunityToolkit.App.Shared/Pages/GettingStartedPage.xaml.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using CommunityToolkit.Tooling.SampleGen.Metadata;
+using Windows.ApplicationModel;
 
 namespace CommunityToolkit.App.Shared.Pages;
 
