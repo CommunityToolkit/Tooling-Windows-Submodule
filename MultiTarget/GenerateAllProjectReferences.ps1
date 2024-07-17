088712d4af263dd3dbbf90172a9d8c460d94b939
Arlo Godfrey <arlo.godfrey@outlook.com>
2024-07-17 12:28:51 -0500

    Fixed an issue where Build-Toolkit-Gallery script wasn't working in Labs

--- MultiTarget/GenerateAllProjectReferences.ps1
+++ MultiTarget/GenerateAllProjectReferences.ps1
@@ -28,8 +28,14 @@ foreach ($componentName in $Components) {
     continue;
   }
 
+  # Don't generate project reference if component isn't available
+  if (!(Test-Path "$PSScriptRoot/../../components/$componentName/")) {
+    continue;
+  }
+
   # Find all components source csproj (when wildcard), or find specific component csproj by name.
   foreach ($componentPath in Get-Item "$PSScriptRoot/../../components/$componentName/") {
+    $componentName = $componentPath.BaseName;
     Write-Output "Generating project references for component $componentName at $componentPath";
 
     # Find source and sample csproj files
