3f2ceba9c4224a50fe7e7e42e0125e768b1b42a5
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2026-02-26 15:59:46 -0800

    Add Powered By Uno Platform graphic and info to WASM sample app
    Can't run WASM all solution, so having trouble validating this change (builds though and doesn't disrupt WASDK head)

--- CommunityToolkit.App.Shared/Pages/SettingsPage.xaml.cs
+++ CommunityToolkit.App.Shared/Pages/SettingsPage.xaml.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Linq;
 using Windows.ApplicationModel;
 
 namespace CommunityToolkit.App.Shared.Pages;
@@ -12,6 +13,11 @@ namespace CommunityToolkit.App.Shared.Pages;
 public sealed partial class SettingsPage : Page
 {
     public string AppVersion => $"Version {Package.Current.Id.Version.Major}.{Package.Current.Id.Version.Minor}.{Package.Current.Id.Version.Build}";
+
+    public string UnoVersion =>
+        AppDomain.CurrentDomain.GetAssemblies()
+            .FirstOrDefault(a => a.GetName().Name == "Uno.UI" || a.GetName().Name == "Uno.WinUI")?
+            .GetName().Version?.ToString() ?? "N/A";
 
     public SettingsPage()
     {
